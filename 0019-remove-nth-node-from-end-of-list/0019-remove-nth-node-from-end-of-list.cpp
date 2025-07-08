d1b2b8138a329913bf466f079e6cdd01dd87ecde
Shreyash Dhas <152175034+shreyashdhas0604@users.noreply.github.com>
2025-07-09 00:36:26 +0530

    Time: 0 ms (100%), Space: 15 MB (59.48%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,21 +11,47 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        //brute force approach - 2 pass
+
+        // ListNode* temp = head;
+        // int size = 0;
+        // while(temp){
+        //     temp = temp->next;
+        //     size++;
+        // }
+
+        // int req = size - n + 1;
+        // temp = head;
+        // if(req == 1){
+        //     return head->next;
+        // }
+        // else{
+        //     for(int i = 1;i<req-1;i++){
+        //         temp = temp->next;
+        //     }
+        //     if(temp->next){
+        //         temp->next = temp->next->next;
+        //     }
+        // }
+
+        // return head;
+
+        //optimal approach - single pass
+
         ListNode* ptr1 = head,*ptr2 = head;
-        while(n--){
-            ptr1 = ptr1->next;
-        }
+            while(ptr1 && n--){
+                ptr1 = ptr1->next;
+            }
 
-        if(!ptr1) return head->next;
+            if(!ptr1) return ptr2->next;
 
-        while(ptr1 && ptr1->next){
-            ptr1 = ptr1->next;
-            ptr2 = ptr2->next;
-        }
+            while(ptr1 && ptr1->next){
+                ptr1 = ptr1->next;
+                ptr2 = ptr2->next;
+            }
 
-        ptr2->next = ptr2->next->next;
+        if(ptr2->next) ptr2->next = ptr2->next->next;
 
         return head;
-
     }
 };
